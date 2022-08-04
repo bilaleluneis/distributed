1344d446ddff7eb7ec5021d8c12e16e5e490df5f
bilaleluneis <bilaleluneis@gmail.com>
2022-08-03 19:14:58 -0700

    introduce error constants

---
+++ common/errors.go
@@ -0,0 +1,18 @@
+/*	@author: Bilal El Uneis
+	@since: August 2022
+	@email: bilaleluneis@gmail.com	*/
+
+package common
+
+// Error credit to https://dave.cheney.net/2016/04/07/constant-errors
+type Error string
+
+func (e Error) Error() string { return string(e) }
+
+const (
+	ReqGrpIdErr          = Error("group id is required")
+	ReqUuidErr           = Error("uuid is required")
+	DoesNotExistGrpIdErr = Error("group id does not exist")
+	NoResultsErr         = Error("no results")
+	NoWorkerAvailErr     = Error("no available workers")
+)
